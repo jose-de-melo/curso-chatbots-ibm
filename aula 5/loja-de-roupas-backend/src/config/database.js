bc3e01bd62ca83f90fe4e03a41898e0660d1dfa2
José de Melo <josedemelo.if@gmail.com>
2019-07-21 16:56:59 -0300

    [UP] Fixing bugs

--- aula 5/loja-de-roupas-backend/src/config/database.js
+++ aula 5/loja-de-roupas-backend/src/config/database.js
@@ -5,5 +5,5 @@ mongoose.Promise = global.Promise
 
 const uriMongo = process.env.MONGODB_URI || 'mongodb://localhost/loja-de-roupas'
 
-module.exports = mongoose.connect(uriMongo, {userMongoClient: true});
+module.exports = mongoose.connect(uriMongo, {useNewUrlParser: true});
 
